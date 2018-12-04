0bf71650ff3483c351806f7dac93c5e890018c82
Jesus Piedrahita <jesus.piedrahita@axiacore.com>
2018-12-04 15:58:23 -0500

    :lollipop: improvement transfom section functionality

--- js/main.js
+++ js/main.js
@@ -7,24 +7,12 @@ $(() => {
         return false;
     });
 
-    document.querySelectorAll('.js-ring-list').forEach(object => {
-        new IntersectionObserver((objects) => {
-            objects.forEach(object => {
-                if (object.isIntersecting) {
-                    $(object.target).addClass('ring-list_active');
-                } else {
-                    $(object.target).removeClass('ring-list_active');
-                }
-            });
-        }, { rootMargin: '-50% 0px' }).observe(object);
-    });
-
     document.querySelectorAll('.js-ring-item').forEach(object => {
         new IntersectionObserver((objects) => {
             objects.forEach(object => {
                 if (object.isIntersecting) {
-                    $('.js-ring-item').find('.ring-item__img-container_active').removeClass('ring-item__img-container_active');
-                    $(object.target).find('.ring-item__img-container').addClass('ring-item__img-container_active');
+                    $('.js-transform-img.transform__img-item_active').removeClass('transform__img-item_active');
+                    $(`.js-transform-img:eq(${$(object.target).index()})`).addClass('transform__img-item_active');
                 }
             });
         }, { rootMargin: '-50% 0px' }).observe(object);
